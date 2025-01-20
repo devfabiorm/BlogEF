c6d5e78ca6b00277ab745a66c1a6c7832ecb009c
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-01-19 23:48:05 -0300

    feat: DataContext configured and first insert on database

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogEF.Models;
+
+[Table("Tag")]
 public class Tag
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BlogEF.Data;
+using BlogEF.Models;
+
+using var context = new BlogDataContext();
+
+var tag = new Tag { Name = "Vue", Slug = "vue" };
+
+context.Tags.Add(tag);
+context.SaveChanges();
