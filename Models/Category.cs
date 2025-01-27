7f0b63c58b7ae23d13d55fe2216f128452eed8d5
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-01-26 23:21:12 -0300

    wip: Mapping using Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogEF.Models;
 
+[Table("Category")]
 public class Category
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
+    [MinLength(3)]
+    [MaxLength(80)]
+    [Column("Name", TypeName = "NVARCHAR")]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [MinLength(3)]
+    [MaxLength(80)]
+    [Column("Name", TypeName = "VARCHAR")]
     public string Slug { get; set; } = string.Empty;
 }
