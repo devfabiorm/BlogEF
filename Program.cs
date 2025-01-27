1a3027300cb5a0d9632da38d50194dee9c1840a5
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-01-27 19:40:35 -0300

    feat: Reaftoring to keep basics methods

--- Program.cs
+++ Program.cs
@@ -4,37 +4,68 @@ using Microsoft.EntityFrameworkCore;
 
 using var context = new BlogDataContext();
 
-// var tag = new Tag { Name = "Vue", Slug = "vue" };
+//AddTag(context);
+//RetrieveTag(context);
+//Update(context);
+//Remove(context);
+//ListTags(context);
+//RetrieveWithoutMetadata(context);
 
-// context.Tags.Add(tag);
-// context.SaveChanges();
+static void AddTag(BlogDataContext context)
+{
+    var tag = new Tag { Name = "Vue", Slug = "vue" };
 
-// var tag = context.Tags.FirstOrDefault(x => x.Id == 1003);
-// tag!.Name = "VueJs";
-// tag.Slug = "vuejs";
+    context.Tags.Add(tag);
+    context.SaveChanges();
+}
 
-// context.Update(tag);
-// context.Tags.Update(tag);
-// context.SaveChanges();
+static void RetrieveTag(BlogDataContext context)
+{
+    var tag = context.Tags.FirstOrDefault(x => x.Id == 1003);
+    Console.WriteLine(tag.Name);
+}
 
-// var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
-// context.Remove<Tag>(tag!);
-// context.Tags.Remove(tag!);
-// context.SaveChanges();
+static void Update(BlogDataContext context)
+{
+    var tag = context.Tags.FirstOrDefault(x => x.Id == 1003);
 
-// var tags = context
-//     .Tags
-//     .AsNoTracking() //Unable tracking by EF, avoing bringing back metadata incresing performance
-//     .Where(x => x.Name.Contains(".NET"))
-//     .ToList();
+    tag!.Name = "VueJs";
+    tag.Slug = "vuejs";
 
-// foreach (var tag in tags)
-// {
-//     Console.WriteLine(tag.Name);
-// }
+    context.Update(tag);
+    //context.Tags.Update(tag);
+    context.SaveChanges();
+}
 
-var tag = context
-    .Tags
-    .AsNoTracking()
-    //.SingleOrDefault() //In case of more than one register, an exception will be throw
-    .FirstOrDefault(x => x.Id == 3);
+static void Remove(BlogDataContext context)
+{
+    var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
+    context.Remove<Tag>(tag!);
+    //context.Tags.Remove(tag!);
+    context.SaveChanges();
+}
+
+static void ListTags(BlogDataContext context)
+{
+    var tags = context
+        .Tags
+        .AsNoTracking() //Disable tracking by EF, avoing bringing back metadata incresing performance
+        .Where(x => x.Name.Contains(".NET"))
+        .ToList();
+
+    foreach (var tag in tags)
+    {
+        Console.WriteLine($"Name: {tag.Name}");
+    }
+}
+
+static void RetrieveWithoutMetadata(BlogDataContext context)
+{
+    var tag = context
+        .Tags
+        .AsNoTracking()
+        //.SingleOrDefault() //In case of more than one register, an exception will be throw
+        .FirstOrDefault(x => x.Id == 3);
+
+    Console.WriteLine($"Name: {tag!.Name}");
+}
