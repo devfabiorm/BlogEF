7f02e070f849ae7ecf81dc34ac385688d8a95509
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-01-29 21:26:21 -0300

    feat: removed data annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,22 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace BlogEF.Models;
 
-[Table("Category")]
 public class Category
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required]
-    [MinLength(3)]
-    [MaxLength(80)]
-    [Column("Name", TypeName = "NVARCHAR")]
     public string Name { get; set; } = string.Empty;
-    [Required]
-    [MinLength(3)]
-    [MaxLength(80)]
-    [Column("Slug", TypeName = "VARCHAR")]
     public string Slug { get; set; } = string.Empty;
 }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,20 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace BlogEF.Models;
 
-[Table("Tag")]
 public class Tag
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required]
-    [MinLength(20)]
-    [MaxLength(80)]
     public string Name { get; set; } = string.Empty;
-    [Required]
-    [MinLength(3)]
-    [MaxLength(80)]
     public string Slug { get; set; } = string.Empty;
 }
