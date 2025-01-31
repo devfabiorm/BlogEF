473327e2dc00d44ea4247b390629946629736d85
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-01-30 22:37:41 -0300

    feat: mapped post class

---
+++ Data/Mappings/PostMap.cs
@@ -0,0 +1,57 @@
+using BlogEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogEF.Data.Mappings;
+
+public class PostMap : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        //Table
+        builder.ToTable("Post");
+
+        //Primary Key
+        builder.HasKey(x => x.Id);
+
+        //Identity
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        //Property
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+
+        builder.Property(x => x.Summary)
+            .IsRequired()
+            .HasColumnType("VARHCAR")
+            .HasMaxLength(255);
+
+        builder.Property(x => x.Body)
+            .IsRequired()
+            .HasColumnType("TEXT");
+
+        builder.Property(x => x.Slug)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.CreateDate)
+            .HasColumnType("SMALLDATETIME")
+            .HasDefaultValueSql("GETDATE()");
+        //.HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+        builder.Property(X => X.LastUpdateDate)
+            .HasColumnType("SMALLDATETIME")
+            //.HasDefaultValueSql("GETDATE()");
+            .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+        //Indexes
+        builder
+            .HasIndex(x => x.Slug, "IX_Post_Slug")
+            .IsUnique();
+    }
+}
