a0c274d2c3874bdf42b4fa9c9960ae824ecf0c92
devfabiorm <fabio.evangelion.guitar@gmail.com>
2025-02-02 23:29:29 -0300

    feat: using ef to generate sql script

---
+++ Scripts/script.sql
@@ -0,0 +1,101 @@
+﻿-- dotnet ef migrations script -o ./script.sql
+
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Category] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] VARCHAR(80) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Roles] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Slug] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Tags] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Slug] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Tags] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [User] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] VARCHAR(200) NOT NULL,
+    [PasswordHash] VARCHAR(255) NOT NULL,
+    [Bio] TEXT NOT NULL,
+    [Image] VARCHAR(2000) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Post] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] VARCHAR(160) NOT NULL,
+    [Summary] VARCHAR(255) NOT NULL,
+    [Body] TEXT NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+    [CreateDate] SMALLDATETIME NOT NULL DEFAULT (GETDATE()),
+    [LastUpdateDate] SMALLDATETIME NOT NULL DEFAULT (GETDATE()),
+    [CategoryId] int NOT NULL,
+    [AuthorId] int NOT NULL,
+    CONSTRAINT [PK_Post] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Post_Author] FOREIGN KEY ([AuthorId]) REFERENCES [User] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Post_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [UserRole] (
+    [RoleId] int NOT NULL,
+    [UserId] int NOT NULL,
+    CONSTRAINT [PK_UserRole] PRIMARY KEY ([RoleId], [UserId]),
+    CONSTRAINT [FK_UserRole_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_UserRole_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [PostTag] (
+    [PostId] int NOT NULL,
+    [TagId] int NOT NULL,
+    CONSTRAINT [PK_PostTag] PRIMARY KEY ([PostId], [TagId]),
+    CONSTRAINT [FK_PostTag_PostId] FOREIGN KEY ([PostId]) REFERENCES [Tags] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_PostTag_TagId] FOREIGN KEY ([TagId]) REFERENCES [Post] ([Id]) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX [IX_Category_Slug] ON [Category] ([Slug]);
+
+CREATE INDEX [IX_Post_AuthorId] ON [Post] ([AuthorId]);
+
+CREATE INDEX [IX_Post_CategoryId] ON [Post] ([CategoryId]);
+
+CREATE UNIQUE INDEX [IX_Post_Slug] ON [Post] ([Slug]);
+
+CREATE INDEX [IX_PostTag_TagId] ON [PostTag] ([TagId]);
+
+CREATE UNIQUE INDEX [IX_User_Slug] ON [User] ([Slug]);
+
+CREATE INDEX [IX_UserRole_UserId] ON [UserRole] ([UserId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250203013842_InitialCreation', N'9.0.1');
+
+ALTER TABLE [User] ADD [GitHub] nvarchar(max) NOT NULL DEFAULT N'';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250203022121_AddedPropertyMigrations', N'9.0.1');
+
+COMMIT;
+GO
+
